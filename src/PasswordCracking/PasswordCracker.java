22682b522c74fa27bc30e322432cb8d057ce1972
Emil Lengman <emillen@kth.se>
2016-05-16 20:32:54 +0200

    getSalt implemented. Is supposed to parse salt from password file

--- src/PasswordCracking/PasswordCracker.java
+++ src/PasswordCracking/PasswordCracker.java
@@ -8,11 +8,18 @@ import java.util.Scanner;
 public class PasswordCracker {
 
 
-    public static ArrayList<String> getWordList(String fileName) throws FileNotFoundException{
+    private static String getSalt(String string) {
+
+        int index = string.indexOf(":") + 1;
+
+        return string.substring(index, index + 2);
+    }
+
+    private static ArrayList<String> getWordList(String fileName) throws FileNotFoundException {
 
         Scanner s = new Scanner(new File("filepath"));
         ArrayList<String> list = new ArrayList<>();
-        while (s.hasNextLine()){
+        while (s.hasNextLine()) {
             list.add(s.nextLine());
         }
         s.close();
