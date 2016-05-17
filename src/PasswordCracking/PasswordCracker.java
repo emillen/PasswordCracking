d3bd0520446905197b47bc88d425f9d027e37e1d
Emil Lengman <emillen@kth.se>
2016-05-17 21:35:55 +0200

    Filling ram with mangles seems to help the speed

--- src/PasswordCracking/PasswordCracker.java
+++ src/PasswordCracking/PasswordCracker.java
@@ -9,22 +9,72 @@ public class PasswordCracker {
 
     public static void main(String[] args) {
 
-        if (args.length != 2) {
+        /*if (args.length != 2) {
             System.out.println("Usage: PasswordCracker <passwordFileName> <dictionaryFileName>");
             return;
-        }
+        }*/
+
+        String dict = "/home/daseel/cracker/dict.txt";
+        String pwdfile = "/home/daseel/cracker/passwd2.txt";
+
         ArrayList<String> wordList;
         ArrayList<Password> passwordList;
 
         try {
-            wordList = getWordList(args[1]);
-            passwordList = getPasswordList(args[0]);
-
+            wordList = getWordList(dict);
+            passwordList = getPasswordList(pwdfile);
         } catch (Exception e) {
 
             System.out.println("File not found");
             return;
         }
+
+
+        addGCOS(passwordList, wordList);
+        System.out.println("Creating le mangles\n");
+        getMangles(wordList);
+        System.out.println("Done with le mangles\n");
+        hackPasswordsBreh(passwordList, wordList);
+    }
+
+    private static void getMangles(ArrayList<String> words){
+
+        ArrayList<String> mangles = new ArrayList<>();
+        for(String s : words)
+            mangles.addAll(Mangler.createMangleList(s));
+
+        words.addAll(mangles);
+
+        for(String s : mangles)
+            words.addAll(Mangler.createMangleList(s));
+    }
+
+    private static void addGCOS(ArrayList<Password> passwords, ArrayList<String> words){
+
+        for(Password pass : passwords)
+            words.addAll(pass.getGCOS());
+    }
+
+    private static void hackPasswordsBreh(ArrayList<Password> passwords, ArrayList<String> words){
+
+        for(String s : words){
+            ArrayList<Password> failed = new ArrayList<>();
+            for(Password pass : passwords){
+                if(isPassphrase(pass, s)){
+                    System.out.println(s);
+                    continue;
+                }
+
+                failed.add(pass);
+            }
+
+            passwords = failed;
+        }
+    }
+
+    private static boolean isPassphrase(Password pass, String passphrase) {
+
+        return JCrypt.crypt(pass.getSalt(), passphrase).equals(pass.getDigestString());
     }
 
     private static ArrayList<Password> getPasswordList(String fileName) throws FileNotFoundException {
